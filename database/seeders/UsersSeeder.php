6fa98f8b75e3fab3fd59b3028222b6e0ae227af5
root <root@sugih-af>
2024-10-28 20:03:46 +0700

    Update

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -12,13 +12,13 @@ class UsersSeeder extends Seeder
 {
     public function run()
     {
-        // Distributor::create([
-        //     'name' => 'Distributor One',
-        //     'email' => 'distributor1@example.com',
-        //     'password' => Hash::make('distributor123'),
-        //     'phone' => '1234567890',
-        //     'address' => '123, Distributor Street, Distributor City',
-        // ]);
+        Distributor::create([
+            'name' => 'Distributor One',
+            'email' => 'distributor1@example.com',
+            'password' => Hash::make('distributor123'),
+            'phone' => '1234567890',
+            'address' => '123, Distributor Street, Distributor City',
+        ]);
         Distributor::create([
             'name' => 'Distributor Two',
             'email' => 'distributor2@example.com',
@@ -27,18 +27,18 @@ class UsersSeeder extends Seeder
             'address' => '125, Distributor Street, Distributor City',
         ]);
 
-        // Supervisor::create([
-        //     'name' => 'Supervisor One',
-        //     'email' => 'supervisor1@example.com',
-        //     'password' => Hash::make('supervisor123'),
-        //     'phone' => '1234567890',
-        //     'address' => '123, Supervisor Street, Supervisor City',
-        // ]);
+        Supervisor::create([
+            'name' => 'Supervisor One',
+            'email' => 'supervisor1@example.com',
+            'password' => Hash::make('supervisor123'),
+            'phone' => '1234567890',
+            'address' => '123, Supervisor Street, Supervisor City',
+        ]);
 
-        // Factory::create([
-        //     'name' => 'Factory One',
-        //     'email' => 'factory1@example.com',
-        //     'password' => Hash::make('factory123'),
-        // ]);
+        Factory::create([
+            'name' => 'Factory One',
+            'email' => 'factory1@example.com',
+            'password' => Hash::make('factory123'),
+        ]);
     }
 }
