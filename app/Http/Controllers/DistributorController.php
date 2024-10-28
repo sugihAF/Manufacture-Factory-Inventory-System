f9583e57d2286aa5a53468ebd56e3216e15a17af
root <root@sugih-af>
2024-10-29 00:26:17 +0700

    Fix some file

--- app/Http/Controllers/DistributorController.php
+++ app/Http/Controllers/DistributorController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\SparepartRequest;
 use App\Models\Invoice;
+use App\Models\Sparepart;
 
 class DistributorController extends Controller
 {
